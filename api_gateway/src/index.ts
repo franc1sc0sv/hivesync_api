aae0cc0ccee927f545ab4a216a2ce87da5fd05e3
franc1sc0sv <franc1sc0.sv.xd@gmail.com>
2024-04-03 13:45:36 -0600

    kaneki

--- api_gateway/src/index.ts
+++ api_gateway/src/index.ts
@@ -17,23 +17,51 @@ app.get("/", async (_, res) => {
   try {
     await prisma.$queryRaw`SELECT 1`;
     return res.status(200).json({
+      message: "Hi from HiveSync API",
       server: 1,
       db: 1,
     });
   } catch (error) {
+    console.log(error)
     if (error instanceof PrismaClientInitializationError) {
       return res.status(200).json({
+        message: "Hi from HiveSync API",
         server: 1,
         db: 0,
       });
     }
     return res.status(200).json({
+      message: "Hi from HiveSync API",
       server: 0,
       db: 0,
     });
   }
 });
 
+app.get("/get_users", async (_, res) => {
+  try {
+    const data = await prisma.user.findMany()
+    return res.json(data)
+  } catch {
+    return res.json({ status: 500, message: "Error del servidor" })
+  }
+})
+
+app.get("/create_user", async (_, res) => {
+  try {
+    const data = await prisma.user.create({
+      data: {
+        email: `lucy${Math.random()}@gmail.com`,
+        name: "Lucy <3"
+      }
+    })
+    return res.json(data)
+  } catch (e) {
+    console.log(e)
+    return res.json({ status: 500, message: "Error del servidor" })
+  }
+})
+
 app.listen(PORT, () => {
-  console.log(`Server initialized in ${PORT}`);
+  console.log(`API GATEWAY initialized in ${PORT}`);
 });
---
+++ social_service/src/index.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaClientInitializationError } from "@prisma/client/runtime/library.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+const prisma = new PrismaClient();
+
+app.get("/", async (_, res) => {
+    try {
+        await prisma.$queryRaw`SELECT 1`;
+        return res.status(200).json({
+            message: "Hi from SOCIAL SERVICE",
+            server: 1,
+            db: 1,
+        });
+    } catch (error) {
+        console.log(error)
+        if (error instanceof PrismaClientInitializationError) {
+            return res.status(200).json({
+                message: "Hi from SOCIAL SERVICE",
+                server: 1,
+                db: 0,
+            });
+        }
+        return res.status(200).json({
+            message: "Hi from SOCIAL SERVICE",
+            server: 0,
+            db: 0,
+        });
+    }
+});
+
+app.get("/get_friends", async (_, res) => {
+    try {
+        const data = await prisma.friends.findMany()
+        return res.json(data)
+    } catch {
+        return res.json({ status: 500, message: "Error del servidor" })
+    }
+})
+
+app.get("/add_friends", async (_, res) => {
+    try {
+        const data = await prisma.friends.create({
+            data: {
+                friend1: 'lucy' + crypto.randomUUID(),
+                friend2: 'FJ' + crypto.randomUUID()
+            }
+        })
+        return res.json(data)
+    } catch (e) {
+        console.log(e)
+        return res.json({ status: 500, message: "Error del servidor" })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`SOCIAL microservice initialized in ${PORT}`);
+});
