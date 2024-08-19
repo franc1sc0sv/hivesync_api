840f1ed824ad9c2beabeb112a35d33f58db06df0
ineedoritos <cgon991@gmail.com>
2024-08-18 20:00:40 -0600

    a
    a

--- api_gateway/src/index.ts
+++ api_gateway/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+import chat_router from "./v1/chat_router";
+
+
+
+
 
 import default_router from "./v1/default_router";
 import auth_router from "./v1/auth_router";
@@ -11,17 +18,22 @@ import {
   UserInfoProxyMiddleware,
 } from "./middleware/microservices";
 import { auth_middleware_microservices } from "./middleware/authForMicroservices";
+import { setupSocketIO } from "./utils/socket";
 
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
 
 app.use(express.json());
 app.use(cors());
 process.env.TZ = "America/El_Salvador";
 
 const PORT = process.env.PORT || 3000;
-
 const BASE_URL = "/api/v1";
+
+// Setup Socket.IO
+setupSocketIO(io);
 
 app.get("/", (_, res) => {
   res.redirect(BASE_URL);
@@ -49,7 +61,9 @@ app.use(BASE_URL + "/channels", [
   auth_middleware_microservices(),
   ChannelsProxyMiddleware,
 ]);
-
-app.listen(PORT, () => {
+// wea
+app.use(BASE_URL + "/chat", chat_router);
+//aca lo cambie a server pero lo wa subir en una rama nueva mejor, no vaya ser
+server.listen(PORT, () => {
   console.log(`API GATEWAY initialized in ${PORT}`);
 });
